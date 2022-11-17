5fd2bca04bbcb27c6d946a7b246c4a350ad658a7
keme-ebi <kelvin6bolou@gmail.com>
2022-11-17 15:46:03 -0800

    code that passes betty checks

---
+++ bettychecks.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * string_rev - reverses a string
+ * @s: char
+ * Return: void
+ */
+
+void string_rev(char *s)
+{
+	char *start, *end, tmp;
+	int len = 0, c;
+
+	while (*(s + len) != '\0')
+		len++;
+	start = s;
+	end = s;
+
+	for (c = 0; c < len - 1; c++)
+		end++;
+	for (c = 0; c < len / 2; c++)
+	{
+		tmp = *end;
+		*end = *start;
+		*start = tmp;
+		start++;
+		end--;
+	}
+}
