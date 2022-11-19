2f01479db6fd0780653954f395a8bf7bf41c77a9
keme-ebi <kelvin6bolou@gmail.com>
2022-11-19 09:35:51 -0800

    remove main.h, and edited bettychecks.c

--- bettychecks.c
+++ bettychecks.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * string_rev - reverses a string
--- main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
-void string_rev(char *s);
-
-#endif /* MAIN_H */
